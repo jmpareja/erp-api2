b7621888090ad5093ce02eecc00b0a14085c0947
jmpareja <jmpareja@gmail.com>
2021-02-28 00:58:48 +0800

    Initial commit

---
+++ src/main/java/com/jmpsolutions/erpapi2/ErpApi2Application.java
@@ -0,0 +1,13 @@
+package com.jmpsolutions.erpapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpApi2Application.class, args);
+	}
+
+}
