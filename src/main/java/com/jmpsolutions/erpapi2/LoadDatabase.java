9d44cdbea81d1ffe7d151e973e2efa5280f9f47f
jmpareja <jmpareja@gmail.com>
2021-02-28 02:54:51 +0800

    Added support for reading json file and loading to H2

--- src/main/java/com/jmpsolutions/erpapi2/LoadDatabase.java
+++ src/main/java/com/jmpsolutions/erpapi2/LoadDatabase.java
@@ -1,11 +1,21 @@
 package com.jmpsolutions.erpapi2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jmpsolutions.erpapi2.entity.Address;
 import com.jmpsolutions.erpapi2.entity.Employee;
 import com.jmpsolutions.erpapi2.repository.AddressRepository;
@@ -16,6 +26,7 @@ class LoadDatabase {
 
 	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 	
+	/*
 	@Bean
 	CommandLineRunner loadDbFromFile(EmployeeRepository empRepository, AddressRepository addrRepository) {
 		Address address1 = new Address("street1", "city1", "zipcode1");
@@ -29,5 +40,43 @@ class LoadDatabase {
 			log.info("Preloading " + empRepository.save(new Employee("joel2", "2021-02-22", "42", "mycompany",
 					"jmpareja@gmail.com", "myphone2", "101", address2)));
 		};
+	}
+	*/
+	@Bean
+	CommandLineRunner loadDbFromFile(EmployeeRepository empRepository, AddressRepository addrRepository) {
+		Resource resData = new ClassPathResource("static/employee.json");
+		String strJson = getFileContents(resData);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			List<Employee> lstEmployee = objectMapper.readValue(strJson, new TypeReference<List<Employee>>() {});
+			log.info("Size of list: " + lstEmployee.size());
+			lstEmployee.forEach(employee -> {
+				Address address = employee.getAddress();
+				if (address != null) {
+					addrRepository.save(employee.getAddress());
+				}
+				empRepository.save(employee);
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return args -> {
+			log.info("Finished loading data."); 
+		};
+		
+	}
+	
+	private String getFileContents(Resource res) {
+		String resData = "";
+        try {
+        	InputStream inputStream = res.getInputStream();
+            byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
+            resData = new String(bdata, StandardCharsets.UTF_8);
+            //log.info(resData);
+        } catch (IOException e) {
+            log.error("IOException", e);
+        }
+        
+        return resData;
 	}
 }
--- src/main/java/com/jmpsolutions/erpapi2/controller/EmployeeController.java
+++ src/main/java/com/jmpsolutions/erpapi2/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.jmpsolutions.erpapi2.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jmpsolutions.erpapi2.entity.Employee;
 import com.jmpsolutions.erpapi2.repository.EmployeeRepository;
 
 @RestController
+@RequestMapping(value="/api")
 public class EmployeeController {
 
 	private final EmployeeRepository repository;
