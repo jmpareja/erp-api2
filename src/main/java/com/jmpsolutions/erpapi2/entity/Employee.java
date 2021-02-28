54de91d921799623a5df19dbebccc46360d85ae9
jmpareja <jmpareja@gmail.com>
2021-03-01 02:01:19 +0800

    Added javadoc comment to Employee entity class

--- src/main/java/com/jmpsolutions/erpapi2/entity/Employee.java
+++ src/main/java/com/jmpsolutions/erpapi2/entity/Employee.java
@@ -61,59 +61,166 @@ public class Employee {
 		this.address = address;
 	}
 
+	/**
+	 * Gets field 'id'.
+	 * 
+	 * @return id
+	 */
 	public Long getId() {
 		return id;
 	}
+	
+	/**
+	 * Sets field 'id' to id.
+	 * 
+	 * @param id value to set
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Gets field 'name'.
+	 * 
+	 * @return name value
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Sets field 'name'.
+	 * 
+	 * @param name value to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Gets field 'joindate'.
+	 * 
+	 * @return joindate Date object
+	 */
 	public Date getJoindate() {
 		return joindate;
 	}
+	
+	/**
+	 * Sets field 'joindate'.
+	 * 
+	 * @param joindate value to set
+	 */
 	public void setJoindate(Date joindate) {
 		this.joindate = joindate;
 	}
+	
+	/**
+	 * Gets field 'age'.
+	 * 
+	 * @return age value
+	 */
 	public Integer getAge() {
 		return age;
 	}
+	
+	/**
+	 * Sets field 'age'.
+	 * 
+	 * @param age value to set
+	 */
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+	
+	/**
+	 * Gets field 'company'.
+	 * 
+	 * @return company value
+	 */
 	public String getCompany() {
 		return company;
 	}
+	
+	/**
+	 * Sets field 'company'.
+	 * 
+	 * @param company value to set
+	 */
 	public void setCompany(String company) {
 		this.company = company;
 	}
+	
+	/**
+	 * Gets field 'email'.
+	 * 
+	 * @return email value
+	 */
 	public String getEmail() {
 		return email;
 	}
+	
+	/**
+	 * Sets field 'email'.
+	 * 
+	 * @param email value to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	/**
+	 * Gets field 'phone'.
+	 * 
+	 * @return phone value
+	 */
 	public String getPhone() {
 		return phone;
 	}
+	
+	/**
+	 * Sets field 'phone'.
+	 * 
+	 * @param phone value to set
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
+	/**
+	 * Gets field 'salary'.
+	 * 
+	 * @return salary value
+	 */
 	public Integer getSalary() {
 		return salary;
 	}
+	
+	/**
+	 * Sets field 'salary'.
+	 * 
+	 * @param salary value to set
+	 */
 	public void setSalary(Integer salary) {
 		this.salary = salary;
 	}
+	
+	/**
+	 * Gets field 'address' object.
+	 * 
+	 * @return Address object
+	 */
 	public Address getAddress() {
 		return address;
 	}
+	
+	/**
+	 * Sets field 'address' to Address object.
+	 * 
+	 * @param address object/instance to set
+	 */
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-		
+
 }
